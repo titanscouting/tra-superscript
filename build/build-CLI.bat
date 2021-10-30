0fd24d42e12a16fc929945cc5cdd6745ce023171
Arthur Lu <learthurgo@gmail.com>
2021-10-30 03:51:03 +0000

    removed testing notebook, anticipated removal of pandas requirement, added pandas and matplotlib to pyinstaller exclusion, removed unessasary option in build scripts

--- build/build-CLI.bat
+++ build/build-CLI.bat
@@ -2,4 +2,4 @@ set pathtospec="../src/cli/superscript.spec"
 set pathtodist="../dist/"
 set pathtowork="temp/"
 
-pyinstaller --onefile --clean --distpath %pathtodist% --workpath %pathtowork% %pathtospec%
+pyinstaller --clean --distpath %pathtodist% --workpath %pathtowork% %pathtospec%
