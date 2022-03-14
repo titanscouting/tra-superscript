8c28c24d60ee00688afdcbb6bb817d4ed213bd0c
Arthur Lu <learthurgo@gmail.com>
2022-03-14 01:33:24 +0000

    removed all unessasary files, moved important files to folder "competition"
    Former-commit-id: 59becb22abc3305a36e2876351e6c7306e3f551e

--- build/build-CLI.bat
+++
@@ -1,5 +0,0 @@
-set pathtospec="../src/superscript.spec"
-set pathtodist="../dist/"
-set pathtowork="temp/"
-
-pyinstaller --clean --distpath %pathtodist% --workpath %pathtowork% %pathtospec%
--- build/build-CLI.sh
+++
@@ -1,5 +0,0 @@
-pathtospec="superscript.spec"
-pathtodist="../dist/"
-pathtowork="temp/"
-
-pyinstaller --clean --distpath ${pathtodist} --workpath ${pathtowork} ${pathtospec}
