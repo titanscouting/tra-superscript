e8a5bb75f80c66f049fcfaa7755855b0ef055d22
Dev <dsingh@imsa.edu>
2022-03-13 21:56:34 -0500

    add sbatch script
    Former-commit-id: f521f7b3f69df71171cd046a40bcbcb6637967a6

---
+++ submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=tra-superscript
+#SBATCH --output=superscript.out
+#SBATCH --ntasks=8
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=256
+#SBATCH --mail-user=dsingh@imsa.edu
+#SBATCH -p cpu-long
+
+unset XDG_RUNTIME_DIR
+/home/tools/anaconda3/bin/conda init
+source ~/.bashrc
+source activate base
+cd competition
+python superscript.py debug
